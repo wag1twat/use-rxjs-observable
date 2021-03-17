4c66a67bc72f1a8ffea2b0ae77ea7ce934d7c799
Шульга Юрий Александрович <yury.shulga@am-intech.ru>
2021-03-17 21:30:39 +0300

    fixed

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+exports.useRxRequest = exports.useRxRequests = void 0;
+var dist_1 = require("./dist");
+exports.useRxRequest = dist_1.useRxRequest;
+exports.useRxRequests = dist_1.useRxRequests;
---
+++ index.ts
@@ -0,0 +1,25 @@
+import {
+  useRxRequest,
+  useRxRequests,
+  RxRequestConfig,
+  RxRequestsSubscriberConfig,
+  RxRequestResult,
+  MultiObservableConfigure,
+  RxMutableRequestConfig,
+  SingleObservableConfigure,
+  UseRxRequestValue,
+  UseRxRequestsValue,
+} from "./dist";
+
+export type {
+  RxRequestConfig,
+  RxRequestsSubscriberConfig,
+  RxRequestResult,
+  MultiObservableConfigure,
+  RxMutableRequestConfig,
+  SingleObservableConfigure,
+  UseRxRequestValue,
+  UseRxRequestsValue,
+};
+
+export { useRxRequests, useRxRequest };
